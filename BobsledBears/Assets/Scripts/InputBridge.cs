5026728d816aa259e57e18cb43478bbad4a3e993
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2023-04-27 14:15:09 -0400

    Mobile-browser input

--- BobsledBears/Assets/Scripts/InputBridge.cs
+++ BobsledBears/Assets/Scripts/InputBridge.cs
@@ -8,6 +8,9 @@ public class InputBridge : MonoBehaviour
     public static bool Left { get; private set; }
     public static bool Right { get; private set; }
 
+    bool LeftTouch = false;
+    bool RightTouch = false;
+
     int count = 0;
 
     // Start is called before the first frame update
@@ -20,9 +23,10 @@ public class InputBridge : MonoBehaviour
     void Update()
     {
         if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || 
-            Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && count == 0)
+            Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S) || LeftTouch) && count == 0)
         {
             Left = true;
+            LeftTouch = false;
         }
         else
         {
@@ -30,14 +34,25 @@ public class InputBridge : MonoBehaviour
             count = 0;
         }
         if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D) || 
-            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && count == 0)
+            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || RightTouch) && count == 0)
         {
             Right = true;
+            RightTouch = false;
         }
         else
         {
             Right = false;
             count = 0;
         }
+    }
+
+    public void TouchLeft()
+    {
+        LeftTouch = true;
+    }
+
+    public void TouchRight()
+    {
+        RightTouch = true;
     }
 }
