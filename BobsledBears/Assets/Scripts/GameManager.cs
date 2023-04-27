87df94a200b4e0f97c24d14b2dd244377a2619a1
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2023-04-27 13:07:34 -0400

    Timer Tweak & WebGL Switch

--- BobsledBears/Assets/Scripts/GameManager.cs
+++ BobsledBears/Assets/Scripts/GameManager.cs
@@ -39,7 +39,7 @@ public class GameManager : MonoBehaviour
             {
                 EndRace();
             }
-            else
+            if (finished.Count < 4)
             {
                 timer += Time.deltaTime;
                 string mins = "" + (timer / 60);
